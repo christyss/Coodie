ebb58656e01088c9ebbbc20d49ce6bac75ef6282
christyss <christy.suenmw@gmail.com>
2021-09-07 04:42:44 -0400

    finished styling

--- client/src/components/SelectButton/SelectButton.js
+++ client/src/components/SelectButton/SelectButton.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 
 
 
+
 function SelectButton() {
     const getRandomId = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,8 +13,7 @@ function SelectButton() {
         <section className="selection-btn">
             
             <Link to={`/mix/${getRandomId(1, 8)}`} className="selection-btn__all">Mix</Link>
-            <Link to={`/asian/${getRandomId(1, 3)}`} className="selection-btn__asian">
-                <img src="../../assets/icons/ramen.jpg" />Asian</Link>
+            <Link to={`/asian/${getRandomId(1, 3)}`} className="selection-btn__asian">Asian</Link>
             <Link to={`/western/${getRandomId(6, 8)}`} className="selection-btn__western">Western</Link>
             <Link to={`/vegan/${getRandomId(4, 5)}`} className="selection-btn__vegan">Vegan</Link>
         </section >
